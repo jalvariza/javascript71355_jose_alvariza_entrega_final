fb650fdc6ace7cb8dc1d29ee579750a630635b75
jalvariza <167147159+jalvariza@users.noreply.github.com>
2024-07-22 09:51:52 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
-function calcularCantidad(totalEsmalte, porcentaje) {
-    return (totalEsmalte / 100) * porcentaje;
+let esmaltes = [];
+
+function calcularCantidad(cantidadEsmalte, sumatoriaPorcentajes, porcentajeElemento) {
+    return (cantidadEsmalte / sumatoriaPorcentajes) * porcentajeElemento;
 }
 
-
 do {
-    let nombreEsmalte = prompt('¡Hola!, te ayudaré a preparar la fórmula para tu esmalte cerámico. Ingresa el nombre de tu esmalte').toUpperCase    ();
+    let nombreEsmalte;
+    while (true) {
+        nombreEsmalte = prompt('Ingresa el nombre del esmalte que quieres formular').toUpperCase();
+        if (nombreEsmalte !== '') {
+            break;
+        }
+        alert('El nombre del esmalte no puede estar vacío. Por favor, ingrésalo nuevamente.');
+    }
 
-    let totalEsmalte = parseInt(prompt('¿Cuántos kilos de esmalte necesitas preparar?'));
-    if (isNaN(totalEsmalte) || totalEsmalte <= 0) { 
-        alert('Debes ingresar un número mayor a 0kg');
-        continue;
-    } else {
-        console.log(`Estas son las cantidades que necesitas para preparar ${totalEsmalte}kg de esmalte ${nombreEsmalte}:`);
-
-        let cantidadElementos = parseInt(prompt('¿Cuántos elementos tiene tu esmalte?'));
-        if (isNaN(cantidadElementos) || cantidadElementos < 2) {
-            alert('Debes ingresar al menos 2 elementos');
-            continue;
+    let cantidadEsmalte = prompt('Ingresa la cantidad de kilos de esmalte que quieres formular');
+        if (isNaN(cantidadEsmalte) || cantidadEsmalte <= 0) { 
+            alert('Debes ingresar un número mayor a 0');
         } else {
-            let x = 1;
-            while (x <= cantidadElementos) {
-                let elemento = prompt(`Ingrese el nombre del Elemento número ${x}`);
-                let porcentaje = parseInt(prompt(`Ingrese el porcentaje del ${elemento}`));
-                if (isNaN(porcentaje) || porcentaje <= 0) {
-                    alert('Debes ingresar un porcentaje mayor a 0');
-                    continue;
+            let cantidadElementos = parseInt(prompt('Ingresa la cantidad de elementos que integran el esmalte'));
+            if (isNaN(cantidadElementos) || cantidadElementos < 2) {
+                alert('Debes ingresar un número igual o mayor a 2');
+            } else {
 
-                } else {
-                    let cantidad = calcularCantidad(totalEsmalte, porcentaje);
-                    console.log(`${elemento} (%${porcentaje}): ${cantidad}kg`);
-                    x++;
-                } 
-            }
+                let elementos = [];
+            
+                for (let i = 0; i < cantidadElementos; i++) {
+                    let elemento = prompt(`Ingresa el nombre del elemento ${i + 1} que compone tu esmalte`);
+
+                    let porcentajeElemento = parseInt(prompt(`Ingresa el porcentaje del elemento ${elemento}`));
+                    if (isNaN(porcentajeElemento) || porcentajeElemento <= 0) { 
+                        alert('Debes ingresar un número mayor a 0');
+                        i--;
+                    } else {
+                        elementos.push({ nombre: elemento, porcentaje: porcentajeElemento });
+                    }
+                }
+            
+            elementos.sort((a, b) => b.porcentaje - a.porcentaje);
+
+            let sumatoriaPorcentajes = elementos.reduce((sum, elemento) => sum + elemento.porcentaje, 0);
+
+            console.log(`Estas son las cantidades que necesitas para preparar ${cantidadEsmalte}kg de esmalte ${nombreEsmalte}:`);
+            elementos.forEach(elemento => {
+                let cantidad = calcularCantidad(cantidadEsmalte, sumatoriaPorcentajes, elemento.porcentaje);
+                let cantidadRedondeada = cantidad.toFixed(2);
+                console.log(`${elemento.nombre}: ${cantidadRedondeada}kg`);
+            });
+
+            esmaltes.push([nombreEsmalte, elementos]);
+
+            const maximoPorcentajeElemento = elementos.find(elemento => elemento.porcentaje === elementos[0].porcentaje);
+            console.log(`Elemento con mayor porcentaje en las recetas es: ${maximoPorcentajeElemento.nombre}, ${maximoPorcentajeElemento.porcentaje}%`);
+            
+
         }
     }
     let continuar = prompt('¿Deseas realizar otro cálculo? (si/no)').toLowerCase();
@@ -39,6 +61,9 @@ do {
         alert('¡Hasta luego!');
         break;   
     }
-
 } while (true);
 
+esmaltes.forEach((esmalte) => {
+    console.log(`Esmalte: ${esmalte[0]}`);
+    console.table(esmalte[1]);
+});
